519637d51b1b5d3a519c7b6d0759e141059ccb16
DocSeattle <seattledraws@gmail.com>
2025-02-07 12:07:31 +0100

    +nav prep, +- css -svg

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,32 @@
+import "./Navigation.css";
+
+export default function Navigation(){
+    const Nav = {
+       home: "#", 
+       solo: "#",
+       versus:"#",
+       leader:"#",
+       login: "#",
+       settings: "#"
+    }
+    return (
+        <>
+            <nav>
+                <div className="left">
+                  <a href={Nav.home}>Type.d <span className="cursor">|</span> </a>
+
+                </div>
+                <div className="middle">
+                  <a href={Nav.solo}>Solo Mode <span className="cursor">|</span></a>
+                  <a href={Nav.versus}>Versus Mode <span className="cursor">|</span></a>
+                  <a href={Nav.leader}>Leaderboards <span className="cursor">|</span></a>
+
+                </div>
+                <div className="right">
+                    <a href={Nav.login}>Login <span className="cursor">|</span> </a>
+                    <a href={Nav.settings}>Settings <span className="cursor">|</span> </a>
+                </div>
+            </nav>
+        </>
+    )
+}
